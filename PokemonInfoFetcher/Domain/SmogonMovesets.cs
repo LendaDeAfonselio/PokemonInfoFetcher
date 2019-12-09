a69774a299db2ffd41664265e6676380867cb0ad
Afonso Falcão <afonso.falcao@cgi.com>
2019-12-09 11:55:27 +0000

    Fixed wrong json parsing

--- PokemonInfoFetcher/Domain/SmogonMovesets.cs
+++ PokemonInfoFetcher/Domain/SmogonMovesets.cs
@@ -8,11 +8,12 @@ namespace PokemonInfoFetcher.Domain
         public string Pokemon { get; set; }
         public int Level { get; set; }
         public string Description { get; set; }
+        public IEnumerable<string> Items { get; set; }
         public IEnumerable<string> Abilities { get; set; }
         public IEnumerable<IEnumerable<string>> Moveslots { get; set; } // TODO - review later
         public IEnumerable<PokemonStats> EVConfigs { get; set; }
         public IEnumerable<PokemonStats> IVConfigs { get; set; }
-        public IEnumerable<string> Nature { get; set; }
+        public IEnumerable<string> Natures { get; set; }
 
     }
 }
